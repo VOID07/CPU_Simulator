24623d5f375cfc328be256dbea58c9bea3ed57f5
Dagoberto Rojas <krojas96ce@gmail.com>
2021-04-19 22:37:21 -0600

    updated folder structure

--- timeBasedBlock.py
+++
@@ -1,9 +0,0 @@
-# Informal interface to inherit the actions to perform when the clock goes up and down
-
-class TimeBasedBlock:
-    # Clock goes up
-    def tic(self): 
-        pass
-    # Clock goes down
-    def toc(self):
-        pass
